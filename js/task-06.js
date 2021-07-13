d6195ed09ed91a500c72f0bc4a6484625b2b705f
Andrey F <media.andrej@gmail.com>
2021-07-13 13:17:22 +0300

    task06 finished

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputField = document.querySelector('#validation-input');
+
+inputField.addEventListener('blur', callBack);
+
+function callBack() {
+    if (inputField.textLength == inputField.dataset.length) {
+        if (inputField.classList.contains('invalid')) inputField.classList.remove('invalid');
+        inputField.classList.add('valid');
+        return;
+    }
+    if (inputField.classList.contains('valid')) inputField.classList.remove('valid');
+    inputField.classList.add('invalid');
+}
